232e51456a60599c63f52738df0f945529588d99
BelenMaru <bleneee@gmail.com>
2022-02-28 21:56:04 -0500

    checking input value

--- src/components/SampleForm.js
+++ src/components/SampleForm.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 function SampleForm() {
+  const [input, setInput] = useState("")
   return (
    <form className= "todo-form">
-     <input className="todo-input" placeholder="Add a list"/>
+     <input onChange ={(e) => console.log(e)} className="todo-input" placeholder="Add a list"/>
      <button className= "sample-btn">Add Lists</button>
    </form>
   )

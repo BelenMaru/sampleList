83899a0d8829077f9278432f844b5c3173ccf8a3
BelenMaru <bleneee@gmail.com>
2022-02-28 21:47:45 -0500

    adding placeholder to our input

--- src/components/SampleForm.js
+++ src/components/SampleForm.js
@@ -3,7 +3,7 @@ import React from 'react'
 function SampleForm() {
   return (
    <form className= "todo-form">
-     <input className="todo-input"/>
+     <input className="todo-input" placeholder="Add a list"/>
      <button className= "sample-btn">Add Lists</button>
    </form>
   )

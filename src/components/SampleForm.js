dc3c10604fd6e05e26748932403973e824442cc0
BelenMaru <bleneee@gmail.com>
2022-02-28 21:58:31 -0500

    changing value of input with state

--- src/components/SampleForm.js
+++ src/components/SampleForm.js
@@ -4,7 +4,7 @@ function SampleForm() {
   const [input, setInput] = useState("")
   return (
    <form className= "todo-form">
-     <input onChange ={(e) => console.log(e)} className="todo-input" placeholder="Add a list"/>
+     <input onChange ={(e) => setInput(e.target.value)} className="todo-input" placeholder="Add a list"/>
      <button className= "sample-btn">Add Lists</button>
    </form>
   )

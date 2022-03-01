06c4cf87981a3ccf5464e1637acabb81ed83652c
BelenMaru <bleneee@gmail.com>
2022-02-28 22:30:51 -0500

    adding a unique id to each todo lists

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
+import React, {useState} from react;
 import "./App.css";
 import SampleForm from "./components/SampleForm";
 
 function App() {
+  const [todos, setTodos] = useState([])
   const addList = (text) => {
+    let id = 1;
+    if(todos.length > 0) {
+      id = todos[0].id + 1
+    }
+
 
   }
   return (
     <div className="sample-lists">
-      <h1>Sample todo Lists</h1>
+      <h1>Sample Todo Lists</h1>
       <SampleForm  addList ={addList}/>
     </div>
   );

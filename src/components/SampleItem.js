6a9d6d6a200fbf25a68b5060529b83e4de93387b
BelenMaru <bleneee@gmail.com>
2022-03-01 20:55:09 -0500

    import react icon for the delete option

--- src/components/SampleItem.js
+++ src/components/SampleItem.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import {RiCloseCircleLine} from "react-icons/ri";
 
 function SampleItem(props) {
   return (
-    <div className= "sample-row">{props.todo.text}</div>
-  )
+    <div className="sample-row">
+      {props.todo.text}
+      <div className="icon-container">
+        <RiCloseCircleLine className= "icon"/>
+      </div>
+    </div>
+  );
 }
 
 export default SampleItem
